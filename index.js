58fa7bb57698b7b7143942e6b165d50c88e05e7e
mirenja <mirenjak@gmail.com>
2022-08-13 08:33:23 +0300

    landing page updates

---
+++ index.js
@@ -0,0 +1,34 @@
+let visa = document.getElementById('inlineFormSelectPref');
+let selectedVisa = visa.options[visa.selectedIndex].value;
+
+
+
+
+function update(){
+    // visa.options[visa.selectedIndex].value = selectedVisa;
+    selectedVisa = visa.options[visa.selectedIndex].value;
+    // maintenance
+    console.log(selectedVisa)
+    
+    visaSelection()
+}
+
+
+function visaSelection(){
+    if (selectedVisa == 'tourist'){
+        touristTasks()
+    }
+    else if (selectedVisa == 'student'){
+        studentTasks()
+    }
+    else{
+        console.log('we will add more')
+    }
+}
+function touristTasks(){
+    
+}
+
+function studentTasks(){
+    console.log('ALa shule tena!')
+}
